1c748a7901e65ba0c789dbef3c390ddcd4730bfc
James Castells <james_castells@hotmail.com>
2013-11-02 18:55:23 -0500

    Eliminado archivo de mas

--- mips1.asm
+++ mips1.asm
@@ -1,8 +1,7 @@
          .data      
 fout:   .asciiz "randomMIPS.txt"      # filename for output
-
-buffer: .space 32
-
+coma: ","
+enter: "\n"
 data:    .word     0 : 1200       # storage for 16x16 matrix of words
          .text
              
@@ -29,10 +28,22 @@ loop:    mult     $s0, $t1       # $s2 = row * #cols  (two-instruction sequence)
          add      $s2, $s2, $s1  # $s2 += column counter
          sll      $s2, $s2, 2    # $s2 *= 4 (shift left 2 bits) for byte offset
          
+         #generar un numero aleatorio de caracteres (entre 1 y 3 caracteres)
+         
+         li  $v0, 42                 # service 42 is to set up the upperbound of the random number
+    	addi $a1, $zero, 3  # loading upperbound
+    	syscall    
+    	addi $t3, $a0, 1  	#t3 va a tener la cantidad de caracteres
+    	
+    	li $t4, 0		#t4 va a ser un contador
+    	
+generar:
+
+    	#generar un numero aleatorio (entre 0 y 9)    	      
         li  $v0, 42                 # service 42 is to set up the upperbound of the random number
     	addi $a1, $zero, 9  # loading upperbound
     	syscall
-    	add $t2, $a0, 48     # saving random int from $a0
+    	add $t2, $a0, 49     # saving random int from $a0
     	
         sw  $t2, data($s2) # store the value in matrix element
  
@@ -41,12 +52,35 @@ loop:    mult     $s0, $t1       # $s2 = row * #cols  (two-instruction sequence)
   	la   $a1, data($s2)   # address of buffer from which to write
   	li   $a2, 1       # hardcoded buffer length
  	syscall            # write to file
+ 	
+ 	add $t4, $t4, 1    #agregar al contador
+ 	bne $t4, $t3, generar
+ 		
   
-         addi     $t2, $t2, 1    # increment value to be stored
+     #    addi     $t2, $t2, 1    # increment value to be stored
          
 #  Loop control: If we increment past last column, reset column counter and increment row counter
 #                If we increment past last row, we're finished.
          addi     $s1, $s1, 1    # increment column counter
+         
+                 bne $s1, $t1, imprimircoma
+                 
+ imprimirenter:
+        li   $v0, 15       # system call for write to file
+  	move $a0, $s6      # file descriptor 
+  	la   $a1, enter   # address of buffer from which to write
+  	li   $a2, 1       # hardcoded buffer length
+ 	syscall            # write to file
+ 	j seguir
+                                                                             
+imprimircoma:
+        li   $v0, 15       # system call for write to file
+  	move $a0, $s6      # file descriptor 
+  	la   $a1, coma   # address of buffer from which to write
+  	li   $a2, 1       # hardcoded buffer length
+ 	syscall            # write to file
+ 	
+ seguir:     	
          bne      $s1, $t1, loop # not at end of row so loop back
          move     $s1, $zero     # reset column counter
          addi     $s0, $s0, 1    # increment row counter
