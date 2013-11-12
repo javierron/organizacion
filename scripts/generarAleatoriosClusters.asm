858cc94874ca5ff014d9a2419676fe3e6d2fb05a
Javier Ron <darkblade_jra@hotmail.com>
2013-11-11 22:32:43 -0500

    renombrando archivo

--- scripts/generarAleatoriosClusters.asm
+++ scripts/generarAleatoriosClusters.asm
@@ -1,356 +1,321 @@
-         .data      
-fout:   .asciiz 	"randomMIPSclusters.txt"      # archivo por escribir
+		.data
+list: 		.space	256
+temp:		.space	16
+comma:		.asciiz	","
+return:		.asciiz	"\n"
+fout:   	.asciiz	"randomMIPSclusters.txt"     	# archivo por escribir
 
-comma: 	.asciiz 	","				# coma para los archivos
-return: .asciiz 	"\n"				# enter para los archivos
+mensaje1: 	.asciiz "==========================\n\nIngrese el numero de dimensiones (max. 4): "
+mensaje2: 	.asciiz "==========================\n\nIngrese el numero de clusters que desea: "
+mensaje3: 	.asciiz "Error! Vuelva a ingresar el numero.\n\n"
+mensaje4: 	.asciiz "==========================\nEl archivo se ha generado. Se llama randomMIPSclusters.txt."
 
-mensaje1: .asciiz "==========================\n\nIngrese el numero de dimensiones (max. 4): "
-mensaje2: .asciiz "==========================\n\nIngrese el numero de clusters que desea: "
-mensaje3: .asciiz "Error! Vuelva a ingresar el numero.\n\n"
-mensaje4: .asciiz "==========================\nEl archivo se ha generado. Se llama randomMIPSclusters.txt."
-
-var0: .word 0					# archivos para convertir numeros a strings
-var1: .word 0					# para convertir archivos
-var2: .word 0
-var3: .word 0
-
-list: .word -9999, -9999, -9999, -9999, -9999 	# guardar puntos centrales en x de clusters
-
-listx: .space 1024
-
-saltos: .word 0
-
-data:    .word     0 : 1200       # lista de 1200 puntos
-         .text
-   
-pedirdimension:
+		.text
+	
+	
+	
+	####### INTERFACE CODE ################################
+	
+	pedirdimension:
                
-         li $v0, 4				# imprimir mensaje 1
-         la $a0, mensaje1
+         li 	$v0, 	4				# imprimir mensaje 1
+         la 	$a0, 	mensaje1
          syscall
          
-         li $v0, 5
+         li 	$v0, 	5
          syscall
          
-         li $a0, 5				# si es mayor que cuatro, repetir
+         li 	$a0, 	5				# si es mayor que cuatro, repetir
          
-         blt $v0, $a0, guardardimension
+         blt 	$v0, 	$a0, 	guardardimension
          
-         li $v0, 4				# imprimir mensaje de error
-         la $a0, mensaje3
+         li 	$v0, 	4				# imprimir mensaje de error
+         la 	$a0, 	mensaje3
          syscall
          
-         j pedirdimension
+         j 	pedirdimension
          
  guardardimension:
  
- 	li $a0, 2
- 	bgt $v0, $a0, guardarfinalmente
+ 	li 	$a0	2
+ 	bgt 	$v0, 	$a0, 	guardarfinalmente
  	
- 	li $v0, 2				# valor default: 2
+ 	li 	$v0, 	2				# valor default: 2
  	
 guardarfinalmente:
  	
-         addi $t4, $v0, 0			# guardar valor
+         addi 	$s0, 	$v0, 	0			# guardar valor
          
 pedirclusters:
          
-         li $v0, 4				# imprimir mensaje 2
-         la $a0, mensaje2
+         li 	$v0, 	4				# imprimir mensaje 2
+         la 	$a0, 	mensaje2
          syscall
          
-         li $v0, 5				
+         li 	$v0, 	5				
          syscall
          
-         li $a0, 6				# si es mayor que cuatro, repetir
+         li 	$a0, 	6				# si es mayor que cuatro, repetir
          
-         blt $v0, $a0, guardarclusters
+         blt 	$v0, 	$a0, 	guardarclusters
          
-         li $v0, 4				# imprimir mensaje de error
-         la $a0, mensaje3
+         li 	$v0, 	4				# imprimir mensaje de error
+         la 	$a0, 	mensaje3
          syscall
          
-         j pedirclusters
+         j 	pedirclusters
          
 guardarclusters:
- 	li $a0, 2
- 	bgt $v0, $a0, guardarfinalmente2
+ 	li 	$a0, 	2
+ 	bgt 	$v0, 	$a0, 	guardarfinalmente2
  	
- 	li $v0, 2				# valor default: 2
+ 	li 	$v0, 	2				# valor default: 2
  	
-guardarfinalmente2:
-         addi $t5,$v0, 0			# pedir valor
-         
-         li $t6,1200
-         
-         div $t6, $t4
-         mflo $t6 
- 
-         addi	$v0,	$v0,	5678
-          
-         addi       $t0, $t6,0        # $t0 = number of rows
-         addi       $t1, $t4,0        # $t1 = number of columns
-         addi 	  $t8, $t5,0       # $t8 = numero de clusters
-         move     $s0, $zero     # $s0 = row counter
-         move     $s1, $zero     # $s1 = column counter
-         move     $t2, $zero     # $t2 = the value to be stored
-  
-    	li   $v0, 13       # system call for open file
-  	la   $a0, fout     # output file name
-  	li   $a1, 1        # Open for writing (flags are 0: read, 1: write)
-  	li   $a2, 0        # mode is ignored
- 	syscall            # open a file (file descriptor returned in $v0)
- 	move $s6, $v0      # save the file descriptor  
- 	
- 	li $t9, -1			#valor central en xy, valor temporal de -1
- 	
- 	div $t0, $t8
- 	mflo $t7	
- 	
- 	sw $t7, saltos	
- 	
- 	
-#  Each loop iteration will store incremented $t1 value into next element of matrix.
-#  Offset is calculated at each iteration. offset = 4 * (row*#cols+col)
-#  Note: no attempt is made to optimize runtime performance!
+ guardarfinalmente2:
+         addi 	$s1,	$v0, 	0			# pedir valor
+	
+	###################### END OF INTERFACE CODE#################################
+	
+	
+	li   	$v0, 	13 				# abriendo el archivo      
+  	la   	$a0, 	fout     
+  	li   	$a1, 	1        
+  	li   	$a2, 	0       
+ 	syscall            
+ 	move 	$s6, 	$v0       
+	
+	
+	#li	$s0,	4				# numero de dimensiones
+	#li	$s1,	5				# numero de clusters
+	li	$s2,	1200				# items 
+	li	$s3,	1000				# distancia entre clusters
+	li	$s4,	26				# rango de clusters
+	
+	li 	$t1,	0				# contador de item de lista
 
-loop:    mult     $s0, $t1       # $s2 = row * #cols  (two-instruction sequence)
-         mflo     $s2            # move multiply result from lo register to $s2
-         add      $s2, $s2, $s1  # $s2 += column counter
-         sll      $s2, $s2, 2    # $s2 *= 4 (shift left 2 bits) for byte offset
-        
-         
-         bne $t9, -1, crearnuevonumero
-         
-       
-        li  $v0, 42                 # service 42 is to set up the upperbound of the random number
-    	addi $a1, $zero, 1000  # loading upperbound
-    	syscall
-    		
-    	addi $t9, $a0, 0		#t9 va a tener el centro
-    	addi $a0, $t9, 0
-    	
-    	la $t6, list
-    	li $t3, 0
-    	#add $t3, $t3, $t3
-    	#add $t3, $t3, $t3
-    	sll $t3, $t3, 2
-    	add $t4, $t3, $t6
-   
-    	
-    	sw $t9, 0($t4)			#guardamos en el arreglo
-    	
-    	j continue
 
-crearnuevonumero:	
- 
-        li  $v0, 42                 # service 42 is to set up the upperbound of the random number
-    	addi $a1, $zero, 100  # loading upperbound
-    	syscall
-    	
-    	#addi $a0, $a0, -50		#a0 tiene numero entre -50 y 50
-    	add $a0, $a0, $t9		#a0 tiene numero original sumado el numero anterior
-   
- 	bgt $a0, 1000, hacerlomil
- 	j continue
- 	
-hacerlomil:
-	
-	li $a0,1000
- 	
- continue:
-    	########################## FUN ################################
-    	
-    	#addi $a0, $t8, 0
-    	
-    	add $t2, $a0, 0
-    	
-    	beq $s1, 1, newnumber			# si la dimension actual recorrida
-    	beq $s1, 3, newnumber			# es uno, tres y cinco,
-    	beq $s1, 5, newnumber			# calcular nuevo numero
-    	j continue2
-    	
-newnumber:
-    	
-        li  $v0, 42                 # service 42 is to set up the upperbound of the random number
-    	addi $a1, $zero, 1000  # loading upperbound
-    	syscall
-    	
-    	abs $a0, $a0
-    	
-    	add $t2, $a0, 0
-    	
- continue2:  	
-  
-  	add $a0, $t2, 0
-  	
-  	bgt $a0, 0, continue3
-  	
-  	li $a0, 0
-  
-  	
- continue3:
-    	
-    	blt $a0, 1000, continue4
-    	
-    	li $a0, 1000
-    	
- continue4:
-    	
-    	
- 	li 	$t3,	10			# imprimir numero en el archivo
-	div	$a0,	$t3
-	mfhi 	$t4
-	mflo	$t5
-	addi	$t4,	$t4,	48
-	sw 	$t4,	var3	
+#################### FUN CODE TO GENERATE CLUSTER CENTERS########################
 
-	div	$t5,	$t3
-	mfhi 	$t4
-	mflo	$t5
-	addi	$t4,	$t4,	48
-	sw 	$t4,	var2
+generateItem:
+	li 	$t0,	0 				# contador de dimensiones
 	
-	div	$t5,	$t3
-	mfhi 	$t4
-	mflo	$t5
-	addi	$t4,	$t4,	48
-	sw 	$t4,	var1
-	
-	div	$t5,	$t3
-	mfhi 	$t4
-	mflo	$t5
-	addi	$t4,	$t4,	48
-	sw 	$t4,	var0
-	
- 	
- 	 li   $v0, 15       # system call for write to file
-  	move $a0, $s6      # file descriptor 
-  	la   $a1, var0   # address of buffer from which to write
-  	li   $a2, 1       # hardcoded buffer length
- 	syscall            # write to file
- 	
- 	 li   $v0, 15       # system call for write to file
-  	move $a0, $s6      # file descriptor 
-  	la   $a1, var1    # address of buffer from which to write
-  	li   $a2, 1       # hardcoded buffer length
- 	syscall            # write to file
- 	
- 	 li   $v0, 15       # system call for write to file
-  	move $a0, $s6      # file descriptor 
-  	la   $a1, var2   # address of buffer from which to write
-  	li   $a2, 1       # hardcoded buffer length
- 	syscall            # write to file
- 	
- 	 li   $v0, 15       # system call for write to file
-  	move $a0, $s6      # file descriptor 
-  	la   $a1, var3   # address of buffer from which to write
-  	li   $a2, 1       # hardcoded buffer length
- 	syscall            # write to file
+generateNumber:
+	li	$v0,	42                		# service 42 is to set up the upperbound of the random number
+    	addi	$a1,	$zero,	950  			# loading upperbound
+    	syscall	
     	
     	
-    	######################## END FUN ##############################   	
+    	la 	$t3,	list				# cargando direccion de arreglo
+    	sll	$t0,	$t0,	2			# multiplicando indice por 4
     	
-    	add $t2, $a0, 48     # saving random int from $a0
-    	 	
-        sw  $t2, data($s2) # store the value in matrix element    
-  
-         addi     $t2, $t2, 1    # increment value to be stored
-         
-#  Loop control: If we increment past last column, reset column counter and increment row counter
-#                If we increment past last row, we're finished.
-         addi     $s1, $s1, 1    # increment column counter
-	beq $s1,$t1, seguir
-               
-         li   $v0, 15       # system call for write to file
+    	mul	$t4,	$s0,	$t1	
+    	mflo	$t4
+    	sll	$t4,	$t4,	2
+    	
+    	add	$t3,	$t4,	$t3
+    	add	$t4,	$t3,	$t0			# sumando indice a la direccion del arreglo
+    	sw	$a0,	0($t4)				# escribiendo en la direccion del arreglo
+    	srl	$t0,	$t0,	2			# restaurando indice
+    	addi	$t0,	$t0,	1			# aumentando indice
+    	beq	$t0,	$s0,	continue		# comprobando que el indice haya llegado a su fin
+    	
+    	j	generateNumber				# jump de regreso si se continua el loop
+    	
+continue:
+
+################### CHECK DISTANCE ##############################
+
+	beq	$t1,	$zero,	skipDistanceCheck
+
+	li	$t9,	0				# acumulador para la distancia
+	li	$t3,	0				# iterador del distance checker
+	
+	
+distanceCheckLoop:
+	li 	$t4,	0				# indice dentro de lista
+	
+	
+	la	$t5,	list
+	mul	$t6,	$t1,	$s0
+	sll	$t6,	$t6,	2
+	add 	$t5,	$t5,	$t6		# $t5 tiene la direccion de la ultima lista
+	
+	
+	la	$t6,	list
+	mul	$t2,	$t3,	$s0
+	sll	$t2,	$t2,	2
+	add	$t6,	$t6,	$t2
+	
+listIterationLoop:
+
+	sll	$t4,	$t4,	2
+	add	$t6,	$t6,	$t4			# $t6 tiene la direccion del item actual
+	add	$t5,	$t5,	$t4
+	srl	$t4,	$t4,	2
+	
+	
+	lw	$t7,	0($t5)
+	lw	$t8,	0($t6)
+	
+	
+	sub	$t7,	$t7,	$t8
+	mul	$t7,	$t7,	$t7
+	
+	add	$t9,	$t9,	$t7
+	
+	addi	$t4,	$t4,	1
+	beq	$t4,	$s0,	exitListIteration
+	j listIterationLoop
+exitListIteration:
+	
+	addi	$t3,	$t3,	1
+	beq	$t3,	$t1,	exitDistanceCheck
+	j distanceCheckLoop
+exitDistanceCheck:
+#################################################################
+
+	blt	$t9,	$s3,	generateItem
+skipDistanceCheck:
+	addi	$t1,	$t1,	1			# aumentando indice
+	beq	$t1,	$s1,	doneCreatingCenters	# comprobando que el indice haya llegado a su fin
+    	
+    	
+    	j	generateItem				# jump de regreso si se continua el loop
+	
+doneCreatingCenters:
+
+#################### END OF CODE TO GENERATE CLUSTER CENTERS########################
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ RELEASED ALL TEMPORALS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+#################### FUN CODE TO GENERATE CLUSTERS########################
+
+					#indice de cluster
+
+	li	$t0,	0				#indice de elemento, not important.
+
+forEachItemPerCluster:
+
+	li	$v0,	42                		# service 42 is to set up the upperbound of the random number
+    	add	$a1,	$zero,	$s1			# loading upperbound
+    	syscall	
+
+	move	$t1,	$a0	
+
+	li	$t3,	0				#indice de elemento dentro de lista
+forEachNumberPerItem:
+
+	li	$v0,	42                		# service 42 is to set up the upperbound of the random number
+    	add	$a1,	$zero,	$s4			# loading upperbound
+    	syscall	
+
+	
+	la	$t4,	list				# accediendo a valor correspondiente en la lista
+	mul	$t5,	$t1,	$s0
+	sll	$t5,	$t5,	2
+	add	$t4,	$t4,	$t5
+	sll	$t3,	$t3,	2
+	add	$t4,	$t4,	$t3
+	srl	$t3,	$t3,	2
+	
+	lw	$t4,	0($t4)
+	li	$t5,	2
+	div	$s4,	$t5
+	mflo	$t5
+	
+	sub	$t4,	$t4,	$t5
+	
+	add	$t4,	$t4,	$a0	
+
+	move	$a0,	$t4
+	li	$v0,	1
+	syscall
+
+########################PRINTING NUMBER TO FILE#################################
+	move	$t6,	$a0
+
+	li	$t9,	0
+printIntLoop:
+	li	$t7,	10
+	
+	div	$t6,	$t7
+	mfhi	$t7
+	addi	$t7,	$t7,	48
+	
+	la	$t8,	temp
+	sll	$t9,	$t9,	2
+	add	$t8,	$t8,	$t9
+	srl	$t9,	$t9,	2
+	sw	$t7,	0($t8)
+	
+	
+ 	
+ 	mflo	$t6
+ 	addi	$t9,	$t9,	1
+ 	beq	$t6,	$zero,	endPrintInt
+ 	j	printIntLoop	
+
+endPrintInt:	
+	li   	$v0,	15      
+  	move 	$a0,	$s6      
+  	la   	$a1, 	temp 
+  	addi	$a1,	$a1,	8
+  	li   	$a2, 	1       
+ 	syscall 
+ 	
+ 	li   	$v0,	15   
+ 	addi	$a1,	$a1,	-4
+ 	syscall
+ 	
+ 	li   	$v0,	15   
+ 	addi	$a1,	$a1,	-4
+ 	syscall
+ 	
+	
+################################################################################
+		
+	addi	$t3,	$t3,	1
+	beq	$t3,	$s0,	endNumberPerItem
+							
+	la	$a0,	comma
+	li	$v0,	4
+	syscall
+	
+	li   $v0, 15       # system call for write to file
   	move $a0, $s6      # file descriptor 
   	la   $a1, comma   # address of buffer from which to write
   	li   $a2, 1       # hardcoded buffer length
- 	syscall            # write to file
-         
-  seguir:       
-         bne      $s1, $t1, loop # not at end of row so loop back
-         move     $s1, $zero     # reset column counter
-         addi     $s0, $s0, 1    # increment row counter
-         
-        bne $s0, $t7, seguir2
-        
-        lw $t2, saltos
-    	
-    	add $t7, $t7, $t2 # $t7 * 2
-    	
-    	beq $s0, $t0, terminar
-    	
-    	li $t2, 0
-    	la $t6, list	
-    	
-    	li  $v0, 42                 # service 42 is to set up the upperbound of the random number
-    	addi $a1, $zero, 1000  # loading upperbound
-    	syscall
-    	
-    	
- crearunnuevonumero:
-         
-    	addi $t3, $t2,0
-    	#add $t3, $t3, $t3
-    	#add $t3, $t3, $t3
-    	sll $t3, $t3, 2
-    	add $t4, $t3, $t6
-    	lw $t3, 0($t4)			#chequeamos en el arreglo
-    	
-    	beq $t3, -9999, guardarnumero	# si llegamos a una posicion con -9999, entonces guardar el numero
+ 	syscall  
 
-    	sub $t3, $a0, $t3
-    	abs $t3, $t3
-    	
-    	blt $t3, 120, encerar		# si el nuevo numero generado esta cercano a otro, crear uno nuevo
-    	
-    	addi $t2, $t2, 1
-    	j crearunnuevonumero
+	
+	j	forEachNumberPerItem
+endNumberPerItem:
 
-encerar:
+	la	$a0,	return
+	li	$v0,	4
+	syscall
 
-	li $t2, 0
-	li  $v0, 42                 # service 42 is to set up the upperbound of the random number
-    	addi $a1, $zero, 1000  # loading upperbound
-    	syscall
-    	
-	j crearunnuevonumero
-
-
-guardarnumero:
-
-  addi $t9, $a0, 0		#t9 va a tener el centro
-
-      	la $t6, list
-      	add $t3, $t2, 0
-    	#add $t3, $t3, $t3
-    	#add $t3, $t3, $t3
-    	sll $t3, $t3, 2
-    	add $t4, $t3, $t6
-    	sw $t9, 0($t4)			#chequeamos en el arreglo
-    	
- seguir2: 	
-
-         li   $v0, 15       # system call for write to file
+	li   $v0, 15       # system call for write to file
   	move $a0, $s6      # file descriptor 
   	la   $a1, return   # address of buffer from which to write
   	li   $a2, 1       # hardcoded buffer length
- 	syscall  
-         
-         bne      $s0, $t0, loop # not at end of matrix so loop back
-         
-terminar:
-           # Close the file 
- 	 li   $v0, 16       # system call for close file
-  	move $a0, $s6      # file descriptor to close
-	syscall            # close file
+ 	syscall 
+
+
+	addi	$t0,	$t0,	1
+	beq	$t0,	$s2,	endItemPerCuster
+	j	forEachItemPerCluster
+endItemPerCuster:
+
+
+
+#################### END OF CODE TO GENERATE CLUSTERS########################
+
 	
-	 li $v0, 4				# imprimir mensaje de exito
-         la $a0, mensaje4
-         syscall
+	
     	
-	
-#  We're finished traversing the matrix.
-         li       $v0, 10        # system service 10 is exit
-         syscall                 # we are outta here.
+    	
+    	
+    	
